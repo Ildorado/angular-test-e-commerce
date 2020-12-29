e65ab26ad74778d5f7f2c9ca8d156b94a1aca013
Ildorado <ildoradogam@gmail.com>
2020-12-29 18:50:46 +0300

    feat: finish pre-deployed app

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,17 +2,20 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    id: 'thisisID1',
   },
   {
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    id: 'thisisID2',
   },
   {
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: '',
+    id: 'thisisID3',
   }
 ];
 
